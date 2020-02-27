57ee2e166d1d2e17676871986141730313af0c67
Pučmeloud <plenca@gmail.com>
2020-02-27 08:45:17 +0100

    Návrat o hodně zpátky

--- index.php
+++ index.php
@@ -41,9 +41,12 @@ and open the template in the editor.
         echo "Použila se magickou metodu ToString. Výsledkem je $matej";
 
         echo "nový řádek větve";
+<<<<<<< HEAD
         echo 'Ukazuji práci s krakenm!';
         echo 'Teď pracuji v test větvi';
         echo 'Hrátky s linuxem!';
+=======
+>>>>>>> parent of 3d18ff8... Přidal jsem řádek.
         ?>
     </body>
 </html>
